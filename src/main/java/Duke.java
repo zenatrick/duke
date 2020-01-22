339cedfe84e710b20dc845520b0eaa960f1a30e1
Chau Teng Le <tenglec@u.nus.edu>
2020-01-23 05:35:12 +0800

    Complete Level-1
    In this initial skeletal version of Duke, it starts by greeting the
    user, simply echos commands entered by the user, and exits when the user
    types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
+/**
+ * A Personal Assistant Chatbot that helps the user to keep track of various things.
+ */
 public class Duke {
+    /** Duke logo. */
+    private static final String logo = "\t  ____        _        \n"
+            + "\t |  _ \\ _   _| | _____ \n"
+            + "\t | | | | | | | |/ / _ \\\n"
+            + "\t | |_| | |_| |   <  __/\n"
+            + "\t |____/ \\__,_|_|\\_\\___|\n";
+
+    /** Horizontal line. */
+    private static final String horLine = "\t____________________________________________________________";
+
+    /**
+     * Runs Duke.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetUser();
+        runEchoCommand();
+    }
+
+    private static void greetUser() {
+        System.out.println(
+                horLine + "\n"
+                + logo + "\n"
+                + "\t Hello! I'm Duke.\n"
+                + "\t What can I do for you?\n"
+                + horLine);
+    }
+
+    private static void runEchoCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(
+                    horLine + "\n"
+                    + "\t " + input + "\n"
+                    + horLine);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(
+                horLine + "\n"
+                        + "\t Bye. Hope to see you again soon!\n"
+                        + horLine);
+
+        scanner.close();
     }
 }
